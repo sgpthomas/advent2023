0ea8abdf9c3fa01c0c113bcf61e212de3028bdc7
Sammy Thomas <sgpthomas@gmail.com>
2023-12-08 20:45:40 -0600

    setup a simple harness + solved day1

---
+++ src/day1.rs
@@ -0,0 +1,60 @@
+use regex::Regex;
+
+use crate::Solution;
+
+pub struct Day1;
+
+impl Day1 {
+    fn parse(input: &str) -> usize {
+        match input {
+            "0" | "zero" => 0,
+            "1" | "one" => 1,
+            "2" | "two" => 2,
+            "3" | "three" => 3,
+            "4" | "four" => 4,
+            "5" | "five" => 5,
+            "6" | "six" => 6,
+            "7" | "seven" => 7,
+            "8" | "eight" => 8,
+            "9" | "nine" => 9,
+            x => panic!("{x} is not a digit"),
+        }
+    }
+
+    /// Return the list of matches starting at every index of the string
+    fn overlapping_search<'a>(regex: &'a Regex, input: &'a str) -> Vec<&'a str> {
+        (0..input.len())
+            .into_iter()
+            .filter_map(|i| regex.find(&input[i..]).map(|m| m.as_str()))
+            .collect()
+    }
+}
+
+impl Solution for Day1 {
+    fn part1(input: &str) -> Option<usize> {
+        Some(
+            input
+                .lines()
+                .map(|line| line.matches(char::is_numeric).collect::<Vec<_>>())
+                .map(|digits| (digits[0], digits[digits.len() - 1]))
+                .map(|(first, last)| format!("{first}{last}").parse::<usize>().unwrap())
+                .sum(),
+        )
+    }
+
+    fn part2(input: &str) -> Option<usize> {
+        let re = Regex::new("(one|two|three|four|five|six|seven|eight|nine|zero|[0-9])").unwrap();
+        Some(
+            input
+                // for each line
+                .lines()
+                // gather all regex matches for that line
+                .map(|line| Self::overlapping_search(&re, line))
+                // pull out the first and last one
+                .map(|digits| (digits[0], digits[digits.len() - 1]))
+                .map(|(first, last)| (Self::parse(first), Self::parse(last)))
+                .map(|(first, last)| first * 10 + last)
+                .sum(),
+        )
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,87 @@
+mod day1;
+
+use std::{fs::File, io::Read, path::PathBuf, time::Instant};
+
+use argh::FromArgs;
+use day1::Day1;
+
+/// Run the solution for a particular day
+#[derive(FromArgs)]
+struct Cmdline {
+    /// which day to use run solutions for
+    #[argh(positional)]
+    day: String,
+
+    /// use short data for testing
+    #[argh(switch)]
+    short: bool,
+}
+
+trait Solution {
+    fn part1(input: &str) -> Option<usize>;
+    fn part2(input: &str) -> Option<usize>;
+    fn run(input: &str, short: bool) {
+        let (part1_sol, part1_time, part2_sol, part2_time) = if short {
+            let (part1_input, part2_input) =
+                input.split_at(input.find("---").expect("Couldn't find divider"));
+            let part2_input = &part2_input[4..];
+
+            let now = Instant::now();
+            let part1_sol = Self::part1(part1_input);
+            let part1_time = now.elapsed();
+
+            let now = Instant::now();
+            let part2_sol = Self::part2(part2_input);
+            let part2_time = now.elapsed();
+
+            (part1_sol, part1_time, part2_sol, part2_time)
+        } else {
+            let now = Instant::now();
+            let part1_sol = Self::part1(input);
+            let part1_time = now.elapsed();
+
+            let now = Instant::now();
+            let part2_sol = Self::part2(input);
+            let part2_time = now.elapsed();
+
+            (part1_sol, part1_time, part2_sol, part2_time)
+        };
+
+        println!("Solution");
+        println!(" Part 1: {part1_sol:?} (took {part1_time:?})");
+        println!(" Part 2: {part2_sol:?} (took {part2_time:?})");
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args: Cmdline = argh::from_env();
+
+    // construct file path from command line arguments
+    let data_path = if args.short {
+        PathBuf::from(format!("inputs/{}-short.txt", args.day))
+    } else {
+        PathBuf::from(format!("inputs/{}.txt", args.day))
+    };
+
+    // make sure that the data exists
+    if !data_path.exists() {
+        panic!("{data_path:?} does not exist!");
+    }
+
+    // actually read data into a string
+    let mut file_handle = File::open(data_path).expect("Unable to open file");
+    let mut input = String::new();
+    file_handle
+        .read_to_string(&mut input)
+        .expect("Unable to read string");
+
+    // print the input if we are using short data
+    if args.short {
+        println!("{input}");
+    }
+
+    match args.day.as_str() {
+        "day1" => Day1::run(&input, args.short),
+        _ => panic!("Unknown day"),
+    }
 }
